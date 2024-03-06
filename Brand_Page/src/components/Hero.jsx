1c6b02787ad30eeb10151604492299759a961376
vishaldhangar <imvishaldhangar@gmail.com>
2024-03-06 23:03:38 +0530

    first commit

---
+++ Brand_Page/src/components/Hero.jsx
@@ -0,0 +1,22 @@
+ const HeroSection =()=>{
+    return(
+        <main className="hero">
+            <div className="hero-content">
+                <h1>YOUR FEET DESERVE THE BEST</h1>
+                <p>
+                    YOUR FEET DESERVE THE BEST ADN WE ARE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE ARE HERE TO HELP YOU WITH OUR SHOES.
+                </p>
+
+                <div className="hero-button">
+                    <button>Shop now</button>
+                    <button className="secondary-btn">Category</button>
+                </div>
+            </div>
+            <div className="hero-image">
+                <img src="/Images/hero-image.png" alt="shoes" />
+            </div>
+        </main>
+    )
+ };
+
+ export default HeroSection;
